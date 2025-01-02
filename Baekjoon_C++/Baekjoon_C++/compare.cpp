de518d2b72fc65de211568e54ec4a958e54efed2
Park Jinseong <144192511+ghdlend@users.noreply.github.com>
2025-01-03 00:44:37 +0900

    compare
    두 수 비교
    점수에 따른 성적

---
+++ Baekjoon_C++/Baekjoon_C++/compare.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+int main(void) {
+	//시험 점수를 입력받아 90 ~100점은 A, 80 ~89점은 B,
+	//70 ~79점은 C, 60 ~69점은 D, 나머지 점수는 F를 출력하는 프로그램
+	/*int score;
+	std::cout << "점수를 입력하시오 : ";
+	std::cin >> score;
+	if (score <= 100 && score >= 90)
+		std::cout << "A" << std::endl;
+	else if (score <= 89 && score >= 80)
+		std::cout << "B" << std::endl;
+	else if (score <= 79 && score >= 70)
+		std::cout << "C" << std::endl;
+	else if (score <= 69 && score >= 60)
+		std::cout << "D" << std::endl;
+	else
+		std::cout << "F" << std::endl;
+	return 0;*/
+
+
+	//두 수를 비교해서 값은 큰 쪽으로 부등호 표시
+	/*int i, j;
+	std::cout << "두 개의 정수를 입력 : ";
+	std::cin >> i >> j;
+	if (i < j)
+		std::cout << "<" << std::endl;
+	else if (i > j)
+		std::cout << ">" << std::endl;
+	else
+		std::cout << "값이 같습니다." << std::endl;
+	return 0;*/
+}
