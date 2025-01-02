360da83cfb78cb0cf68081b5e796cb0195d5aeba
Park Jinseong <144192511+ghdlend@users.noreply.github.com>
2025-01-03 00:29:04 +0900

    사칙연산_주석

--- Baekjoon_C++/Baekjoon_C++/arithmetic_operation.cpp
+++ Baekjoon_C++/Baekjoon_C++/arithmetic_operation.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 int main(void) {
 	int i, j;
 	std::cout << "두 개의 정수를 입력 : ";
@@ -8,4 +8,4 @@ int main(void) {
 	std::cout << "두 개의 차는 : " << i - j << std::endl;
 	std::cout << "두 개의 곱은 : " << i * j << std::endl;
 	std::cout << "두 개의 나눈 값은 : " << i / j << std::endl;
-}
+}*/
