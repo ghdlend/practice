551ca893c90824b1c6ebceb6b0637f0d593d3ce0
Park Jinseong <144192511+ghdlend@users.noreply.github.com>
2025-01-02 23:58:22 +0900

    compare
    두 수 비교하기
    점수에 따른 성적

---
+++ Baekjoon_C/Baekjoon_C/compare.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void) {
+
+
+
+
+	//시험 점수를 입력받아 90 ~100점은 A, 80 ~89점은 B,
+	//70 ~79점은 C, 60 ~69점은 D, 나머지 점수는 F를 출력하는 프로그램
+	/*int score;
+	printf("점수를 입력하시오 : ");
+	scanf("%d", &score);
+
+	if (score >= 90 && score <= 100)
+		printf("A");
+	else if (score >= 80 && score <= 89)
+		printf("B");
+	else if (score >= 70 && score <= 79)
+		printf("C");
+	else if (score >= 60 && score <= 69)
+		printf("D");
+	else
+		printf("F");
+	return 0;*/
+
+
+	//두 수 비교하기
+	//값이 큰 쪽으로 부등호 
+	/*int i,j;
+	printf("값을 입력하시오 : ");
+	scanf("%d %d", &i, &j);
+
+	if (i == j)
+		printf("==");
+	else if (i < j)
+		printf("<");
+	else
+		printf(">");*/
+}
