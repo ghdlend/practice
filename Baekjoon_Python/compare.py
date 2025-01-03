6e55affc2980d96b04fabcde1c311c10b02458a8
Park Jinseong <144192511+ghdlend@users.noreply.github.com>
2025-01-03 23:33:27 +0900

    compare
    두 수 비교
    시험 점수에 따른 성적 배분
    윤년
    좌표에 따른 사분면

---
+++ Baekjoon_Python/compare.py
@@ -0,0 +1,41 @@
+# #1.두 수 비교
+# i = int(input("첫 번째 수 입력 : "))
+# j = int(input("두 번째 수 입력 : "))
+# if(i>j):
+#     print(">")
+# elif(i<j):
+#     print("<")
+# else:
+#     print("=")
+# #2.시험 점수에 따른 성적 배분
+# score = int(input("점수 입력 : "))
+# if(score<=100 and score>=90):
+#     print("A")
+# elif(score<=100 and score>=90):
+#     print("B")
+# elif(score<=100 and score>=90):
+#     print("C")
+# elif(score<=100 and score>=90):
+#     print("D")
+# else:
+#     print("F")
+#3.윤년 연도에서 4로 나뉘어 떨어지면서 100으로는 안 떨어지는 수 또는 400으로 나뉘어 떨어지는 수
+# year = int(input("연도를 입력하시오 :"))
+# if((year % 4 == 0 and year % 100 != 0) or year % 400 == 0):
+#     print("윤년")
+# else:
+#     print("윤년이 아님")
+#4.사분면 
+x = int(input("x좌표를 입력해주시오 : "))
+y = int(input("y좌표를 입력해주시오 : "))
+
+if(x > 0 and y > 0):
+    print("1사분면")
+elif(x < 0 and y > 0):
+    print("2사분면")
+elif(x < 0 and y < 0):
+    print("3사분면")
+elif(x > 0 and y < 0):
+    print("4사분면")
+else:
+    print("값을 잘 못 입력하였습니다.")
