6206292ce25238d61e63fb081f27ea8fd08e7a58
Park Jinseong <144192511+ghdlend@users.noreply.github.com>
2025-01-07 02:06:39 +0900

    repeating_sentence
    구구단

--- Baekjoon_C++/Baekjoon_C++/compare.cpp
+++ Baekjoon_C++/Baekjoon_C++/compare.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-int main(void) {
+//int main(void) {
 	//예로, 좌표가 (12, 5)인 점 A는 x좌표와 y좌표가 모두 양수이므로 제1사분면에 속한다. 
 	//점 B는 x좌표가 음수이고 y좌표가 양수이므로 제2사분면에 속한다.
 	//점의 좌표를 입력받아 그 점이 어느 사분면에 속하는지 알아내는 프로그램을 작성하시오.
@@ -60,4 +60,4 @@ int main(void) {
 	else
 		std::cout << "값이 같습니다." << std::endl;
 	return 0;*/
-}
+//}
