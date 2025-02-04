0cb95658972a7fcd8f61237ca09e509dacfd4e07
Park Jinseong <144192511+ghdlend@users.noreply.github.com>
2025-02-04 21:49:14 +0900

    Java 문자열 입력받고 출력하기

---
+++ programmers_java/java_practice/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Input_String = new Scanner(System.in);
+        String a = Input_String.next();
+        System.out.println(a);
+    }
+}
