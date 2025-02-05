74bb16d4b49e1d542ccbbe18435869f4b028f13c
Park Jinseong <144192511+ghdlend@users.noreply.github.com>
2025-02-05 21:26:55 +0900

    문자열 변환

---
+++ programmers_java/java_practice/src/In_Output.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class In_Output {
+//    Scanner Input_String = new Scanner(System.in);
+//    String a = Input_String.next();
+//    System.out.println(a);
+}
--- programmers_java/java_practice/src/Main.java
+++ programmers_java/java_practice/src/Main.java
@@ -1,9 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner Input_String = new Scanner(System.in);
-        String a = Input_String.next();
-        System.out.println(a);
+        String input = "";
+        String output = "";
+        int tmp;
+
+        Scanner scanner = new Scanner(System.in);
+        input = scanner.nextLine();
+
+        for(int i =0; i < input.length();i++){
+            tmp = (int)input.charAt(i);
+            if((65 <=tmp)&&(tmp <= 90)){
+                output += (char)(tmp + 32);
+            }
+            else if((97 <= tmp) && (tmp <= 122)){
+                output += (char)(tmp - 32);
+            }
+            else{
+                output += (char)tmp;
+            }
+        }
+        System.out.println("변환된 문자열 : " + output);
     }
 }
