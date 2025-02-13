5b4102ef9e700b9e4e4bf014829a0f6884a8f1ab
Park Jinseong <144192511+ghdlend@users.noreply.github.com>
2025-02-13 16:59:39 +0900

    별찍기

--- programmers_java/java_practice/src/Main.java
+++ programmers_java/java_practice/src/Main.java
@@ -1,13 +1,7 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        String my_string = "He32knkrld";
-        String overwrite_string = "llowo";
-        int s = 2;
 
-        String result = sol.solution(my_string, overwrite_string, s);
-        System.out.println(result);
     }
 }
 
--- programmers_java/java_practice/src/Soulution.java
+++ programmers_java/java_practice/src/Soulution.java
@@ -1,3 +1,19 @@
+//import java.util.Scanner;
+//public class Main{
+//    public static void main(String[] args) {
+//        Solution sol = new Solution();
+//        String my_string = "He32knkrld";
+//        String overwrite_string = "llowo";
+//        int s = 2;
+//
+//        String result = sol.solution(my_string, overwrite_string, s);
+//        System.out.println(result);
+//    }
+//}
+
+
+
+
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         String answer = "";
@@ -6,7 +22,6 @@ class Solution {
         if (my_string.length() - answer.length() > 0)
             answer += my_string.substring(s + overwrite_string.length());
         return answer;
-
 
     }
 }
