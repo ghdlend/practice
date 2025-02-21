c8d78c7d8d641479e03c74eb2a9fce5171c40d88
Park Jinseong <144192511+ghdlend@users.noreply.github.com>
2025-02-22 03:26:54 +0900

    삽입정렬

---
+++ Algorithm/src/InsertSort.java
@@ -0,0 +1,22 @@
+//import java.util.Arrays;
+//public class InsertSort {
+//    public static void insertSort(int arr[]){
+//        for(int i = 0;i<arr.length;i++){
+//            int key = arr[i];
+//            int j = i -1;
+//
+//            while(j >= 0 && arr[j] > key){
+//                arr[j+1] = arr[j];
+//                j--;
+//            }
+//            arr[j+1] = key;
+//        }
+//    }
+//    public static void main(String[] args){
+//        int[] numbers = {2,4,3,10,1,9};
+//
+//        insertSort(numbers);
+//
+//        System.out.println(Arrays.toString(numbers));
+//    }
+//}
--- Algorithm/src/LinearSearch.java
+++ Algorithm/src/LinearSearch.java
@@ -1,23 +1,23 @@
-public class LinearSearch {
-    public static int linearSearch(int[] arr, int target) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    public static void main(String[] args) {
-        int[] numbers = {10,20,30,40,50};
-        int target = 20;
-
-        int result = linearSearch(numbers,target);
-
-        if(result != -1){
-            System.out.print("찾는값은 : "+ target + "이고 위치는 " + (result+1));
-        }
-        else{
-            System.out.println("찾는 값은:"+target+"없습니다.");
-        }
-    }
-}
+//public class LinearSearch {
+//    public static int linearSearch(int[] arr, int target) {
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] == target) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+//    public static void main(String[] args) {
+//        int[] numbers = {10,20,30,40,50};
+//        int target = 20;
+//
+//        int result = linearSearch(numbers,target);
+//
+//        if(result != -1){
+//            System.out.print("찾는값은 : "+ target + "이고 위치는 " + (result+1));
+//        }
+//        else{
+//            System.out.println("찾는 값은:"+target+"없습니다.");
+//        }
+//    }
+//}
